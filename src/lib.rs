6d806e86d22822348ccde5a65205c2519f19c456
Connor Postma <connor.postma@gmail.com>
2019-04-19 18:05:14 -0400

    working on a run count benchmark
    fixed some stuff in the named benchmark and started work on runcount benchmark and some tests. Currently incomplete

--- src/lib.rs
+++ src/lib.rs
@@ -13,21 +13,71 @@ macro_rules! benchmarknamed {
     (
         #[$($name:ident)?]
         $(
-            $(#[$inner:ident $($args:tt)*])*
             $lines:stmt;
         )*
     ) => {
             let start = Instant::now();
             $($lines;)*
             let duration = start.elapsed();
-            println!("{} time: {:?} | {}", stringify!($($name)?), duration, stringify!(args));
-        };
+            println!("{} time: {:?}", stringify!($($name)?), duration);
+        }
+}
+
+#[macro_export]
+macro_rules! runcount {
+    (
+        #[$($name:ident)?,$timesec:expr,$timenano:expr]
+        $($lines:stmt;)*
+    ) => {
+        let dur = Duration::new($timesec, $timenano)
+        let start = Instant::now();
+        let mut count = 0;
+        loop {
+            count += 1;
+        $($lines;)*
+        if start.elapsed() >= dur {
+                let totaldur = start.elapsed();
+                println!("ran {} for {} times in {:?} time.", stringify!($($name)?), count, totaldur);
+                break;
+            }
+        }
+    };
 }
 
 #[cfg(test)]
 mod tests {
+
+    use std::time::{Instant, Duration};
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_benchmark() {
+        //assert!(stringify!(benchmark!{}) contains "let start = Instant::now();");
+        //TODO CHECK FOR VALID OUTPUT
+    }
+    #[test]
+    fn test_benchmark_named() {
+        //assert!(stringify!(benchmark!{}) contains "let start = Instant::now();");
+        //TODO CHECK FOR VALID OUTPUT
+        benchmarknamed! {
+            #[IhaveAname]
+            for x in 0..10 {
+                let y = x * 2;
+            };
+        }
+    }
+
+    #[test]
+    fn test_runcount() {
+        let mut x = 1;
+        runcount! {
+            #[testcount, 1, 0]
+            let x = x * 2;
+        }
+        assert_eq!("", "ran testcount for 14222626 times in 1.000000281s time.");
+        
+        assert!(match "" {
+            "ran testcount for 14222626 times in 1.000000281s time." => true,
+            _ => false,
+        });
     }
 }
